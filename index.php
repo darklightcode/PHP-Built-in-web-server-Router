077036b962110e98c9891445bfa1ddc7ce4b109b
iustin <bgt.iustin@yahoo.com>
2017-07-16 18:31:36 +0300

    images and css moved to assets and router got moved to router.class.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>PHP-Built-in-web-server-Router</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
-    <link href="src/css/style.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
 </head>
 <?php
 $bing = json_decode(file_get_contents("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"), true);
@@ -16,19 +16,15 @@ $image = '//bing.com/' . $bing["images"][0]["url"];
 <body style="background:url('<?php echo $image ?>')">
 <div class="main">
 
-    <pre class="pre-block">
     <?php
-    print_r($_SERVER);
     console_output('This message will appear only in console ' . date('Y-m-d H:i:s'), array("this" => array("vector" => "too")));
     ?>
-    </pre>
 
     <div class="images-block">
-        <h4 style="margin:0;">Check the following images in Developer Tools > Networking to see http status.</h4>
-        <hr/>
-        <img class="img" src="<?php echo 'http://' . $_SERVER['HTTP_HOST'] ?>/src/images/1.jpg"/>
-        <img class="img" src="<?php echo 'http://' . $_SERVER['HTTP_HOST'] ?>/src/images/2.jpg"/>
-        <div class="last-image">Loaded from style.css</div>
+        <h4 class="info-dev">Check the following images in Developer Tools > Networking to see http status.</h4>
+        <img class="img" src="<?php echo 'http://' . $_SERVER['HTTP_HOST'] ?>/assets/images/1.jpg"/>
+        <img class="img" src="<?php echo 'http://' . $_SERVER['HTTP_HOST'] ?>/assets/images/2.jpg"/>
+        <div class="img last-image">Loaded from style.css</div>
     </div>
 
 
